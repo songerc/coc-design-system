6d7af43aee62f6af597070dd4c975a6209818213
songerc <songerc@charlottesville.org>
2019-11-25 15:51:08 -0500

    button updates

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -14,28 +14,44 @@ import injectSheet from 'react-jss';
 const styles = theme => ({
 
     // All top level object keys are different class names
-    defaultButton: {
-        // Global color applied from theme
-        color: theme.default.buttonColor,
-        backgroundColor: 'transparent',
-        borderColor: theme.default.buttonColor,
-        margin: 0,
-        display: 'inline-block',
-        textAlign: 'center',
-        whiteSpace: 'nowrap',
-        border: '1px solid transparent',
-        fontSize: '1rem',
-        fontWeight: 400,
-        padding: '.375rem .75rem'
-    }
+    'blue-default': theme.button.blue.default,
+    'blue-outline': theme.button.blue.outline,
+    'blue-text': theme.button.blue.text,
+    'orange-default': theme.button.orange.default,
+    'orange-outline': theme.button.orange.outline,
+    'orange-text': theme.button.orange.text,
+    'danger-default': theme.button.danger.default,
+    'danger-outline': theme.button.danger.outline,
+    'danger-text': theme.button.danger.text,
+    'success-default': theme.button.success.default,
+    'success-outline': theme.button.success.outline,
+    'success-text': theme.button.success.text,
+    'link-blue-default': theme.button.link.blue.default,
+    'link-blue-outline': theme.button.link.blue.outline,
+    'link-blue-text': theme.button.link.blue.text,
+    'link-orange-default': theme.button.link.orange.default,
+    'link-orange-outline': theme.button.link.orange.outline,
+    'link-orange-text': theme.button.link.orange.text,
+    'link-danger-default': theme.button.link.danger.default,
+    'link-danger-outline': theme.button.link.danger.outline,
+    'link-danger-text': theme.button.link.danger.text,
+    'link-success-default': theme.button.link.success.default,
+    'link-success-outline': theme.button.link.success.outline,
+    'link-success-text': theme.button.link.success.text,
 });
 
 // Define the component using these styles and pass it to the classes prop
 // use this to assign scoped class names
-const Button = ({classes, children, handleClick}) => (
-    <button className={classes.defaultButton} onClick={() => handleClick()}>
-        {children}
-    </button>
+const Button = ({classes, children, handleClick, variant, root, url}) => (
+    (url === undefined ? (
+        <button className={variant ? classes[root + '-' + variant] : classes[root + '-default']} onClick={() => handleClick()}>
+            {children}
+        </button>
+    ) : (
+        <a href={url} className={variant ? classes['link-' + root + '-' + variant] : classes['link-' + root + '-default']}>
+            {children}
+        </a>
+    ))
 );
 
 // export component with HOC (higher order component) to apply styles from above
--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -7,13 +7,63 @@ import theme from '../../theme/theme';
 import Button from './Button';
 
 storiesOf('Button', module)
-    .add('with text', () => (
+    .add('Blue Base Outline', () => (
         <ThemeProvider theme={theme}>
-            <Button handleClick={action('clicked')}>Hello Button</Button>
+            <Button handleClick={action('clicked')} variant='outline' root='blue'>Blue Outline</Button>
         </ThemeProvider>
     ))
-    .add('with some emoji', () => (
+    .add('Blue Base Filled', () => (
         <ThemeProvider theme={theme}>
-            <Button handleClick={action('emoji clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+            <Button handleClick={action('filled clicked')} root='blue'>Blue Filled</Button>
         </ThemeProvider>
     ))
+    .add('Blue Base Text button', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('text button')} url='https://google.com' root='blue' variant='text'>Blue Text</Button>
+        </ThemeProvider>
+    ))
+    .add('Orange Base Outline', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('clicked')} variant='outline' root='orange'>Orange Outline</Button>
+        </ThemeProvider>
+    ))
+    .add('Orange Base Filled', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('filled clicked')} root='orange'>Orange Filled</Button>
+        </ThemeProvider>
+    ))
+    .add('Orange Base Text button', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('text button')} root='orange' variant='text'>Orange Text</Button>
+        </ThemeProvider>
+    ))
+    .add('Danger Base Outline', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('clicked')} variant='outline' root='danger'>Danger Outline</Button>
+        </ThemeProvider>
+    ))
+    .add('Danger Base Filled', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('filled clicked')} root='danger'>Danger Filled</Button>
+        </ThemeProvider>
+    ))
+    .add('Danger Base Text button', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('text button')} root='danger' variant='text'>Danger Text</Button>
+        </ThemeProvider>
+    ))
+    .add('Success Base Outline', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('clicked')} variant='outline' root='success'>Success Outline</Button>
+        </ThemeProvider>
+    ))
+    .add('Success Base Filled', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('filled clicked')} url='https://www.google.com' root='success'>Success Filled</Button>
+        </ThemeProvider>
+    ))
+    .add('Success Base Text button', () => (
+        <ThemeProvider theme={theme}>
+            <Button handleClick={action('text button')} root='success' variant='text'>Success Text</Button>
+        </ThemeProvider>
+    ));
